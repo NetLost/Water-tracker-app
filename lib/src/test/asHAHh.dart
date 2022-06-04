d9db85c3f3251c4790ce3359772d2daa335296b8
Sanlovty <sanlovty@gmail.com>
2022-06-05 02:17:15 +0300

    chore: trying to make this dirty

--- lib/src/test/asHAHh.dart
+++ lib/src/test/asHAHh.dart
@@ -1,3 +1,14 @@
-class dsdaa {
-  final int? a = null;
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class smallWidget extends StatelessWidget {
+  const smallWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(child: Text("1"))
+    );
+  }
 }
